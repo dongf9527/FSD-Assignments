e243971c7732c58ad708cc7b71561a48c18f84d9
dongf9527 <dongf9527@163.com>
2019-07-21 23:12:58 +0800

    add

---
+++ FSD-03/src/VideoPlayer.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Player from './Player';
+import Controls from './Controls';
+import Playlist from './Playlist';
+
+class VideoPlayer extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { progressValue: 0, currentVideoId: 1 }
+  }
+
+  onTimeUpdateHandler(value) {
+    this.setState({ progressValue: value });
+  }
+
+  playlistClickHandler(value) {
+    this.setState({ currentVideoId: value });
+    console.log("video player currentVideoId", value);
+  }
+
+  refPlayer = React.createRef();
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-7" style={{ textAlign: 'center' }}>
+            <h1>CUSTOMIZED VIDEO PLAYER</h1>
+            <h5>Auther: wpeter@cn.ibm.com</h5>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-7">
+            <Player ref={this.refPlayer} onTimeUpdate={(value) => { this.onTimeUpdateHandler(value) }}></Player>
+            <Controls player={this.refPlayer} progressValue={this.state.progressValue} currentVideoId={this.state.currentVideoId}></Controls>
+          </div>
+          <div className="col-lg-5" id="landscapePlayList" >
+            <Playlist player={this.refPlayer} playlistClick={(value) => { this.playlistClickHandler(value) }}></Playlist>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default VideoPlayer;
